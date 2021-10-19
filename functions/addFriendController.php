47a6fc510987b94a0b1fb19dc7d0c8dc83ae1807
Alexandre Motbal <71082499+AlexandreMotbal@users.noreply.github.com>
2021-10-19 17:59:47 +0200

    Update addFriendController.php

--- functions/addFriendController.php
+++ functions/addFriendController.php
@@ -1,11 +1,5 @@
 <?php
 require_once 'utilisateurController.php';
 
-if(isset($_SESSION['id']) && isset($_POST['friend_mail'])){
-    $idUser = _getId($_SESSION['id']);
-    $friend_mail = htmlspecialchars(mysqli_real_escape_string($_ENV['conn'], $_POST['friend_mail']));
-    $idAdd = _getIdByMail($friend_mail);
-    addFriend($idUser, $idAdd);
-}
+addFriend();
 ?>
-
